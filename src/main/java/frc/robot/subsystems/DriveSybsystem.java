9002df7e8cf769be0b162f61ce0a61673a19a49c
Kai Collins <kaicollins11261@gmail.com>
2024-02-06 15:09:52 -0500

    maybe using ucrl

--- src/main/java/frc/robot/subsystems/DriveSybsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSybsystem.java
@@ -19,16 +19,16 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import static edu.wpi.first.units.MutableMeasure.mutable;
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
+//import static edu.wpi.first.units.MutableMeasure.mutable;
+//import static edu.wpi.first.units.Units.Meters;
+//import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Volts;
-import edu.wpi.first.units.Velocity;
-import edu.wpi.first.units.Distance;
+//import edu.wpi.first.units.Velocity;
+//import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.MutableMeasure;
+//import edu.wpi.first.units.MutableMeasure;
 import edu.wpi.first.units.Voltage;
-import edu.wpi.first.wpilibj.RobotController;
+//import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -61,48 +61,61 @@ private AHRS gyro = new AHRS(SPI.Port.kMXP);
 private DifferentialDriveOdometry driveOdometry;
 private Pose2d drivePose = new Pose2d(new Translation2d(0,0), new Rotation2d(0));
 
-// Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
-private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
-// Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
-private final MutableMeasure<Distance> m_distance = mutable(Meters.of(0));
-// Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
-private final MutableMeasure<Velocity<Distance>> m_velocity = mutable(MetersPerSecond.of(0));
+// Create the SysId routine
+private final SysIdRoutine sysIdRoutine = new SysIdRoutine(
+  new SysIdRoutine.Config(), //we are not using advantage kit so we can just leave this empty
+  new SysIdRoutine.Mechanism(
+    (Measure<Voltage> volts) -> {
+      leftFrontMotor.setVoltage(volts.in(Volts));
+      rightFrontMotor.setVoltage(volts.in(Volts));
+    },
+    null, // No log consumer, since data is recorded by URCL
+    this
+  )
+);
 
-private final SysIdRoutine sysIdRoutine =
-      new SysIdRoutine(
-          // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
-          new SysIdRoutine.Config(),
-          new SysIdRoutine.Mechanism(
-              // Tell SysId how to plumb the driving voltage to the motors.
-              (Measure<Voltage> volts) -> {
-                leftFrontMotor.setVoltage(volts.in(Volts));
-                rightFrontMotor.setVoltage(volts.in(Volts));
-              },
-              // Tell SysId how to record a frame of data for each motor on the mechanism being
-              // characterized.
-              log -> {
-                // Record a frame for the left motors.  Since these share an encoder, we consider
-                // the entire group to be one motor.
-                log.motor("drive-left")
-                    .voltage(
-                        m_appliedVoltage.mut_replace(
-                            leftFrontMotor.get() * RobotController.getBatteryVoltage(), Volts))
-                    .linearPosition(m_distance.mut_replace(this.getAvgLeftPosition(), Meters))
-                    .linearVelocity(
-                        m_velocity.mut_replace(this.getAvgLeftVelocity(), MetersPerSecond));
-                // Record a frame for the right motors.  Since these share an encoder, we consider
-                // the entire group to be one motor.
-                log.motor("drive-right")
-                    .voltage(
-                        m_appliedVoltage.mut_replace(
-                            rightFrontMotor.get() * RobotController.getBatteryVoltage(), Volts))
-                    .linearPosition(m_distance.mut_replace(this.getAvgRightPosition(), Meters))
-                    .linearVelocity(
-                        m_velocity.mut_replace(this.getAvgRightPosition(), MetersPerSecond));
-              },
-              // Tell SysId to make generated commands require this subsystem, suffix test state in
-              // WPILog with this subsystem's name ("drive")
-              this));
+// // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
+// private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
+// // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+// private final MutableMeasure<Distance> m_distance = mutable(Meters.of(0));
+// // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+// private final MutableMeasure<Velocity<Distance>> m_velocity = mutable(MetersPerSecond.of(0));
+
+// private final SysIdRoutine sysIdRoutine =
+//       new SysIdRoutine(
+//           // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
+//           new SysIdRoutine.Config(),
+//           new SysIdRoutine.Mechanism(
+//               // Tell SysId how to plumb the driving voltage to the motors.
+//               (Measure<Voltage> volts) -> {
+//                 leftFrontMotor.setVoltage(volts.in(Volts));
+//                 rightFrontMotor.setVoltage(volts.in(Volts));
+//               },
+//               // Tell SysId how to record a frame of data for each motor on the mechanism being
+//               // characterized.
+//               log -> {
+//                 // Record a frame for the left motors.  Since these share an encoder, we consider
+//                 // the entire group to be one motor.
+//                 log.motor("drive-left")
+//                     .voltage(
+//                         m_appliedVoltage.mut_replace(
+//                             leftFrontMotor.get() * RobotController.getBatteryVoltage(), Volts))
+//                     .linearPosition(m_distance.mut_replace(this.getAvgLeftPosition(), Meters))
+//                     .linearVelocity(
+//                         m_velocity.mut_replace(this.getAvgLeftVelocity(), MetersPerSecond));
+//                 // Record a frame for the right motors.  Since these share an encoder, we consider
+//                 // the entire group to be one motor.
+//                 log.motor("drive-right")
+//                     .voltage(
+//                         m_appliedVoltage.mut_replace(
+//                             rightFrontMotor.get() * RobotController.getBatteryVoltage(), Volts))
+//                     .linearPosition(m_distance.mut_replace(this.getAvgRightPosition(), Meters))
+//                     .linearVelocity(
+//                         m_velocity.mut_replace(this.getAvgRightPosition(), MetersPerSecond));
+//               },
+//               // Tell SysId to make generated commands require this subsystem, suffix test state in
+//               // WPILog with this subsystem's name ("drive")
+//               this));
 
   /** Creates a new DriveSybsystem. */
   public DriveSybsystem() {
